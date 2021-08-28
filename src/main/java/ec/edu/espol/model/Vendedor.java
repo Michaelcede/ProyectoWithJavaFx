93b994f54b02d374886b62ccc26794d7cd0b464a
Michaelcede <74023391+Michaelcede@users.noreply.github.com>
2021-08-28 18:44:51 -0500

    Agregamos el método GuardarVendedor
    Agregue GuardarVendedor
    Agregue la interfaz Serializable

--- src/main/java/ec/edu/espol/model/Vendedor.java
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -5,6 +5,10 @@
  */
 package ec.edu.espol.model;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -12,10 +16,18 @@ import java.security.NoSuchAlgorithmException;
  *
  * @author Dell
  */
-public class Vendedor extends Persona{
+public class Vendedor extends Persona implements Serializable{
     
     public Vendedor(String nombres, String apellidos, String correo, String organizacion, String clave) {
         super(nombres, apellidos, correo, organizacion, clave);
+    }
+    
+    public static void GuardarVendedor(Vendedor v, String nomFile){
+        try(ObjectOutputStream out= new ObjectOutputStream(new FileOutputStream(nomFile))){
+            out.writeObject(v);
+        }catch(IOException ex){
+            System.out.println("IOException");
+        }
     }
     
 }
