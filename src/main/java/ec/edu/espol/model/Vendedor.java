15d394b8b50ff9cd4313f88245da8042aa33dfd0
Michaelcede <74023391+Michaelcede@users.noreply.github.com>
2021-09-02 00:44:26 -0500

    Borre Librerias
    Las que llamamos y no usamos, esas borre

--- src/main/java/ec/edu/espol/model/Vendedor.java
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -5,19 +5,13 @@
  */
 package ec.edu.espol.model;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 /**
